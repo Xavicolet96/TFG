35ea3d95329c18e70a57878e2c21d8ea02780c0b
Xavicolet96 <fxavier.colet@gmail.com>
2019-11-10 12:33:19 +0100

    Mountain fold en proces

--- DataStructure.cs
+++ DataStructure.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class DataStructure 
@@ -7,7 +6,10 @@ public class DataStructure
     public FaceTree faceTree;
     public EdgeBinaryTreeList edgeBTL;
     public VertexLinkedList vertexLL;
+
     public int step;
+    public float epsilon = 0.05f;
+    public Vector3 mountainVec = new Vector3(0, 0, -1);
    
     public DataStructure() {
         
@@ -80,6 +82,158 @@ public class DataStructure
     {
         return vertexLL.getVertices();
     }
+
+    public void mountainFoldVtoV( Vertex v1, Vertex v2)
+    {
+        Vector3 normal = v1.position - v2.position;
+        Vector3 midpoint = (v1.position + v2.position) / 2;
+
+        Vector3 delta = mountainVec * epsilon;
+
+        normal = normal + delta;
+
+        Plane p = new Plane(normal, midpoint);
+
+        getAfecctedVertices(p, v2);
+
+        generateNewCreases(p);
+
+    }
+    public void generateNewCreases(Plane p)
+    {
+        List <EdgeNode> activeEdgeNodes = edgeBTL.getLeaveNodes();
+        List<Vertex> activeVertices = getVertices();
+
+        List<Vertex> cutVertices = new List<Vertex>();
+        /// si tallo un vertex el tinc en compte d(etermineSide =0)
+        for (int i = 0; i < activeVertices.Capacity; i++)
+        {
+            if (p.determineSide(activeVertices[i])==0)
+            {
+                cutVertices.Add(activeVertices[i]);
+            }
+        }
+
+        /// per cada aresta que es talla faig split i genero un vertex
+        for (int i = 0; i< activeEdgeNodes.Capacity; i++)
+
+        {
+            EdgeNode parent = activeEdgeNodes[i];
+            Vertex o = parent.getEdge().origin;
+            Vertex e = parent.getEdge().end;
+
+
+            Vector3 point = parent.getEdge().isCut(p);
+            if (point != Vector3.negativeInfinity)
+            {
+                Vertex v = new Vertex(point);
+                cutVertices.Add(v);
+                vertexLL.addVertex(v);
+
+                Edge edgel = new Edge(o, v);
+                EdgeNode ls = new EdgeNode(parent,edgel);
+
+                Edge edger = new Edge(v, e);
+                EdgeNode rs = new EdgeNode(parent, edger);
+
+                v.addEdge(edgel);
+                v.addEdge(edger);
+
+                
+                parent.setSons(ls, rs);
+
+            }
+        }
+
+        /// per cada vertex que comparteix cara 2 a 2 genero una aresta
+        for (int i = 0; i < cutVertices.Capacity; i++ )
+        {
+            for (int j = 0; j < cutVertices.Capacity; j++)
+            {
+                if (i != j)
+                {
+                    FaceNode f = shareFace(cutVertices[i], cutVertices[j]);
+                    if (f != null)
+                    {
+                        Edge e = new Edge(cutVertices[i], cutVertices[j]);
+                        cutVertices[i].addEdge(e);
+                        cutVertices[j].addEdge(e);
+              
+                        edgeBTL.addEdge(e);
+
+                        /// genero cares
+
+                        Face f1 = new Face();
+                        Face f2 = new Face();
+
+                        FaceNode fn1 = new FaceNode(f,f1);
+                        FaceNode fn2 = new FaceNode(f, f2);
+
+                        f.addSon(fn1);
+                        f.addSon(fn2);
+                    }
+                }
+            }
+        }
+        /// genero cares
+        
+    }
+
+    public FaceNode shareFace(Vertex v1, Vertex v2)
+    {
+        FaceNode f = null;
+        List<FaceNode> faceNodes = getFaceNodes();
+        for (int i = 0; i< faceNodes.Capacity; i++)
+        {
+            f = faceNodes[i];
+            if (f.getFace().has(v1) && f.getFace().has(v2))
+            {
+                return f;
+            }
+        }
+        return f;
+    }
+
+    public List<Vertex> getAfecctedVertices(Plane p, Vertex v)
+    {
+        List <Vertex> vertices = getVertices();
+        int side = p.determineSide(v);
+        for (int i = 0; i< vertices.Capacity; i++)
+        {
+            if (p.determineSide(vertices[i]) == side) {
+                vertices.Add(vertices[i]);
+            }
+        }
+        return vertices;
+    }
+
+    public List<FaceNode> getFaceNodes()
+    {
+        List<FaceNode> list = new List<FaceNode>();
+        list.AddRange(faceTree.getFaceNodes());
+        return list;
+    }
+}
+
+public class Plane {
+    public float A,B,C,D;
+
+    public Plane(Vector3 normal, Vector3 p) 
+    {
+        A = normal.x;
+        B = normal.y;
+        C = normal.z;
+        D = A * p.x + B * p.y + C * p.z;
+    }
+
+    public int determineSide(Vertex v)
+    {
+        if (A * v.position.x + B * v.position.y + C * v.position.z == D) return 0;
+        if (A * v.position.x + B * v.position.y + C * v.position.z < D) return -1;
+        else return 1;
+
+    }
+
 }
 
 public class FaceTree
@@ -99,6 +253,21 @@ public class FaceTree
         List<Face> faces = new List<Face>();
         faces.AddRange(root.getLeaves());
         return faces; 
+    }
+
+    public List<FaceNode> getFaceNodes()
+    {
+        List<FaceNode> list = new List<FaceNode>();
+        if (!root.getHasSons())
+        {
+            list.Add(root);
+            return list;
+        }
+        else
+        {
+            list.AddRange(root.getLeaveNodes());
+        }
+        return list;
     }
 
 }
@@ -137,6 +306,11 @@ public class FaceNode
         return parent;
     }
 
+    public bool getHasSons()
+    {
+        return hasSons;
+    }
+
     public List<Face> getLeaves() {
         List<Face> leaves = new List<Face>();
         if (hasSons){
@@ -152,8 +326,25 @@ public class FaceNode
         }
         return leaves;
     }
+    public List<FaceNode> getLeaveNodes()
+    {
+        List<FaceNode> leaves = new List<FaceNode>();
+        for (int i = 0; i<sons.Capacity;i++)
+        {
+            if (!sons[i].getHasSons())
+            {
+                leaves.Add(sons[i]);
+            }
+            else
+            {
+                sons[i].getLeaveNodes();
+            }
+        }
+        return leaves;
+    }
 
 }
+
 public class EdgeBinaryTreeList
 {
     List<EdgeBinaryTree> edgeBTL;
@@ -177,6 +368,16 @@ public class EdgeBinaryTreeList
         }
         return list;
     } 
+
+    public List<EdgeNode> getLeaveNodes()
+    {
+        List<EdgeNode> list = new List<EdgeNode>();
+        for (int i = 0; i < edgeBTL.Capacity; i++)
+        {
+            list.AddRange(edgeBTL[i].getLeaveNodes());
+        }
+        return list;
+    }
 }
 
 public class EdgeBinaryTree {
@@ -189,10 +390,23 @@ public class EdgeBinaryTree {
     public List<Edge> getLeaves()
     {
         List<Edge> leaves = new List<Edge>();
-
-        
-
+        leaves.AddRange(root.getLeaves());
         return leaves;
+    }
+    public List<EdgeNode> getLeaveNodes()
+    {
+        List<EdgeNode> list = new List<EdgeNode>();
+
+        if (!root.getHasSons())
+        {
+            list.Add(root);
+            return list;
+        }
+        else
+        {
+            list.AddRange(root.getLeaveNodes());
+        }
+        return list;
     }
 }
 
@@ -220,6 +434,11 @@ public class EdgeNode {
         list.Add(leftSon);
         list.Add(rightSon);
         return list;
+    }
+
+    public bool getHasSons()
+    {
+        return hasSons;
     }
 
     public EdgeNode getParent()
@@ -253,6 +472,33 @@ public class EdgeNode {
         }
         return leaves;
     }
+
+    public List<EdgeNode> getLeaveNodes()
+    {
+        List<EdgeNode> leaves = new List<EdgeNode>();
+
+        if (!leftSon.getHasSons())
+        {
+            leaves.Add(leftSon);
+        }
+        else
+        {
+            leftSon.getLeaveNodes();
+        }
+
+        if (!rightSon.getHasSons())
+        {
+            leaves.Add(rightSon);
+        }
+        else
+        {
+            rightSon.getLeaveNodes();
+        }
+
+        return leaves;
+    }
+
+
 }
 
 public class VertexLinkedList
@@ -308,8 +554,8 @@ public class Vertex
     public Vector3 getPosition() {
         return position;
     }
-
 }
+
 public class Edge
 {
     public Vertex origin;
@@ -323,7 +569,37 @@ public class Edge
         end = e;
     }
 
+    public Vector3 isCut (Plane p)
+    {
+        // r = t(end-origin) + origin
+        // A(t(end.x-origin.x)+origin.x) +B(t(end.y-origin.y)+origin.y) +C(t(end.z-origin.z)+origin.z)  = D
+        // A*t*end.x-A*t*origin.x +A*origin.x
+        // t(Aend-Aorigin)...= D-Aorigin-....
+
+        float t;
+        float para = (p.A * (end.position.x - origin.position.x) + p.B * (end.position.y - origin.position.y) + p.C * (end.position.z - origin.position.z));
+        if (para!= 0)
+        {
+            t = (p.D - p.A * origin.position.x - p.B * origin.position.y - p.C * origin.position.z) / para;
+        }
+        else
+        {
+            return Vector3.negativeInfinity;
+        }
+
+        if (t<1 && t > 0)
+        {
+            return t * (end.position - origin.position) + origin.position;
+        }
+        else
+        {
+            return Vector3.negativeInfinity;
+        }
+        //return Vector3.negativeInfinity;
+    }
+
 }
+
 public class Face
 {
     List<Vertex> vList;
@@ -335,5 +611,31 @@ public class Face
         eList = el;
     }
 
-}
+    public List<Vertex>  getVertices() {
+        return vList;
+    }
 
+    public bool has(Vertex v)
+    {
+        for (int i = 0; i < vList.Capacity; i++)
+        {
+            if (v == vList[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool has(Edge v)
+    {
+        for (int i = 0; i < eList.Capacity; i++)
+        {
+            if (v == eList[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
